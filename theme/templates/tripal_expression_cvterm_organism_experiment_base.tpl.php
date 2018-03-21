b838f33e9a221e7e294bdbeddf6d69626bd1b60a
Robb, Sofia <sofia2341@gmail.com>
2018-03-21 17:31:09 -0600

    trying out gene name word list

--- theme/templates/tripal_expression_cvterm_organism_experiment_base.tpl.php
+++ theme/templates/tripal_expression_cvterm_organism_experiment_base.tpl.php
@@ -96,45 +96,7 @@ $script = <<<EOD
     });
   </script>
 EOD;
-$wordcloud_stuff = <<<EOD
-<script>
-//var fill =  d3.scale.category20c();
-var fill = d3.scale.linear()
-    .domain([0, 6])
-    .range(["#def2d9", "#469834"]);
 
-var layout = d3.layout.cloud()
-    .size([500, 500])
-    .words([{"text":"study","size":40},{"text":"motion","size":15},{"text":"forces","size":10},{"text":"electricity","size":15},{"text":"movement","size":10},{"text":"relation","size":5},{"text":"things","size":10},{"text":"force","size":5},{"text":"ad","size":5},{"text":"energy","size":85},{"text":"living","size":5},{"text":"nonliving","size":5},{"text":"laws","size":15},{"text":"speed","size":45},{"text":"velocity","size":30},{"text":"define","size":5},{"text":"constraints","size":5},{"text":"universe","size":10},{"text":"physics","size":120},{"text":"describing","size":5},{"text":"matter","size":90},{"text":"physics-the","size":5},{"text":"world","size":10},{"text":"works","size":10},{"text":"science","size":70},{"text":"interactions","size":30},{"text":"studies","size":5},{"text":"properties","size":45},{"text":"nature","size":40},{"text":"branch","size":30},{"text":"concerned","size":25},{"text":"source","size":40},{"text":"google","size":10},{"text":"defintions","size":5},{"text":"two","size":15},{"text":"grouped","size":15},{"text":"traditional","size":15},{"text":"fields","size":15},{"text":"acoustics","size":15},{"text":"optics","size":15},{"text":"mechanics","size":20},{"text":"thermodynamics","size":15},{"text":"electromagnetism","size":15},{"text":"modern","size":15},{"text":"extensions","size":15},{"text":"thefreedictionary","size":15},{"text":"interaction","size":15},{"text":"org","size":25},{"text":"answers","size":5},{"text":"natural","size":15},{"text":"objects","size":5},{"text":"treats","size":10},{"text":"acting","size":5},{"text":"department","size":5},{"text":"gravitation","size":5},{"text":"heat","size":10},{"text":"light","size":10},{"text":"magnetism","size":10},{"text":"modify","size":5},{"text":"general","size":10},{"text":"bodies","size":5},{"text":"philosophy","size":5},{"text":"brainyquote","size":5},{"text":"words","size":5},{"text":"ph","size":5},{"text":"html","size":5},{"text":"lrl","size":5},{"text":"zgzmeylfwuy","size":5},{"text":"subject","size":5},{"text":"distinguished","size":5},{"text":"chemistry","size":5},{"text":"biology","size":5},{"text":"includes","size":5},{"text":"radiation","size":5},{"text":"sound","size":5},{"text":"structure","size":5},{"text":"atoms","size":5},{"text":"including","size":10},{"text":"atomic","size":10},{"text":"nuclear","size":10},{"text":"cryogenics","size":10},{"text":"solid-state","size":10},{"text":"particle","size":10},{"text":"plasma","size":10},{"text":"deals","size":5},{"text":"merriam-webster","size":5},{"text":"dictionary","size":10},{"text":"analysis","size":5},{"text":"conducted","size":5},{"text":"order","size":5},{"text":"understand","size":5},{"text":"behaves","size":5},{"text":"en","size":5},{"text":"wikipedia","size":5},{"text":"wiki","size":5},{"text":"physics-","size":5},{"text":"physical","size":5},{"text":"behaviour","size":5},{"text":"collinsdictionary","size":5},{"text":"english","size":5},{"text":"time","size":35},{"text":"distance","size":35},{"text":"wheels","size":5},{"text":"revelations","size":5},{"text":"minute","size":5},{"text":"acceleration","size":20},{"text":"torque","size":5},{"text":"wheel","size":5},{"text":"rotations","size":5},{"text":"resistance","size":5},{"text":"momentum","size":5},{"text":"measure","size":10},{"text":"direction","size":10},{"text":"car","size":5},{"text":"add","size":5},{"text":"traveled","size":5},{"text":"weight","size":5},{"text":"electrical","size":5},{"text":"power","size":5}])
-    .padding(5)
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .font("Impact")
-    .fontSize(function(d) { return d.size; })
-    .on("end", draw);
-
-
-layout.start();
-
-function draw(words) {
-  d3.select("#wordcloud").append("svg")
-      .attr("width", layout.size()[0])
-      .attr("height", layout.size()[1])
-    .append("g")
-      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-    .selectAll("text")
-      .data(words)
-    .enter().append("text")
-      .style("font-size", function(d) { return d.size + "px"; })
-      .style("font-family", "Impact")
-      .style("fill", function(d, i) { return fill(i); })
-      .attr("text-anchor", "middle")
-      .attr("transform", function(d) {
-        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-      })
-      .text(function(d) { return d.text; });
-}
-</script>
-EOD;
 //drupal_add_js(drupal_get_path('module', 'tripal_expression_cvterm_organism') . '/theme/js/d3.v4.min.js');
 drupal_add_js(drupal_get_path('module', 'tripal_expression_cvterm_organism') . '/theme/js/d3.v3.min.js');
 //drupal_add_js(drupal_get_path('module', 'tripal_expression_cvterm_organism') . '/theme/js/d3-cloud/build/d3.layout.cloud.js');
@@ -252,7 +214,7 @@ function tripal_expression_cvterm_organism_getExperiment_byGene($results,$image_
 	<br><br>
 	';
 
-
+    $wordcloud_array = array();
 	foreach ($results['genes'] as $item){
 	  $name = $item['name'];
 	  $uniquename = $item['uniquename'];
@@ -280,6 +242,7 @@ function tripal_expression_cvterm_organism_getExperiment_byGene($results,$image_
 		$images[] = "<div class=\"caption_container\"><a href=\"$image_dir/$uri\"><img style=\"width:100%\" class=\"caption_image\" src=\"$image_dir/$uri\" ></a><div class=\"caption_middle\"><div class=\"caption_text\">$caption</div></div></div>";
 	 }
 	 $each_image_count = count($images);
+	 $wordcloud_array[]="{\"text\":\"$name\",\"size\":$each_$image_count}";
 	 $columns = partition($images,4);
 	 $content .= '
 	 <fieldset class=" collapsible collapsed">
@@ -308,7 +271,50 @@ function tripal_expression_cvterm_organism_getExperiment_byGene($results,$image_
 	}
 
 	$content .= "<p><a href=\"#top\">back to top</a></p><br>";
-	return $content;
+$wordcloud_words = join(',',$wordcloud_array);
+dpm($wordcloud_words);
+$wordcloud = <<<EOD
+<script>
+
+var word_freqs = [$wordcloud_words];
+var fill = d3.scale.linear()
+    .domain([0, 6])
+    .range(["#def2d9", "#469834"]);
+
+var layout = d3.layout.cloud()
+    .size([500, 500])
+    .words(word_freqs)
+    .padding(5)
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw);
+
+
+layout.start();
+
+function draw(words) {
+  d3.select("#wordcloud").append("svg")
+      .attr("width", layout.size()[0])
+      .attr("height", layout.size()[1])
+    .append("g")
+      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+    .selectAll("text")
+      .data(words)
+    .enter().append("text")
+      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-family", "Impact")
+      .style("fill", function(d, i) { return fill(i); })
+      .attr("text-anchor", "middle")
+      .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function(d) { return d.text; });
+}
+</script>
+EOD;
+
+	return $content, $wordcloud;
 }
 
 function tripal_expression_cvterm_organism_getExperiment_byTerm($results,$image_count,$term_count,$image_dir){
