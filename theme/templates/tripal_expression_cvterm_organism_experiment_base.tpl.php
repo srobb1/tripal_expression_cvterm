9d4458b70cb4e92cb4b60d677af90222cd73fd5b
Robb, Sofia <sofia2341@gmail.com>
2018-03-22 14:07:30 -0600

    added event

--- theme/templates/tripal_expression_cvterm_organism_experiment_base.tpl.php
+++ theme/templates/tripal_expression_cvterm_organism_experiment_base.tpl.php
@@ -325,6 +325,8 @@ d3.select("#wordcloud").append("svg")
                 .duration(200)		
                 .style("opacity", .9);
         div.html(d.text)
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px");	
       })
       .on("mouseout", function(d){
            div.transition()		
