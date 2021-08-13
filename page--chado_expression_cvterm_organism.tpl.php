28c69320865bc5898be0583b13e872270a5d9b7d
Sofia Robb <sofia2341@gmail.com>
2021-08-13 14:06:27 -0600

    Create page--chado_expression_cvterm_organism.tpl.php

---
+++ page--chado_expression_cvterm_organism.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+dpm($results);
+print("HI");
+// page title be genus species term
+// list the images with this term
+// list all genes that reference this term
+// subdivide by author
+// subdivide by experiment
+// list of experimetns that reference this term
+
+?>
